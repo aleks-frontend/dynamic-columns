93000b64d9f0c83462b03fc431a1a059663e817c
Aleksandar Gojkovic <gojkovic-frontend@hotmail.com>
2020-05-28 13:33:58 +0200

    chore: updated dynamicColumnSetup

--- dynamic-columns-setup.js
+++ dynamic-columns-setup.js
@@ -1,72 +1,69 @@
-
 function dynamicColumnSetup() {
-    var dynamicColumns = document.querySelectorAll("[dynamic-columns]")
-    var htmlTags = "h1, h2, h3, h4, h5, h6, p";
+    var dynamicColumns = document.querySelectorAll("[dynamic-columns]");
+    var htmlTags = "h1, h2, h3, h4, h5, h6, p, ul, ol";
 
-    dynamicColumns.forEach((dynamicColumn) => {
-        var minColumns = dynamicColumn.getAttribute("min-columns");
-        var maxColumns = dynamicColumn.getAttribute("max-columns");
-        var htmlTagElements = dynamicColumn.querySelector(".original-content").querySelectorAll(htmlTags);
-        var breakPointIndexes = [];
+    dynamicColumns.forEach((dynamicColumn) =>{
+      var minColumns = dynamicColumn.getAttribute("min-columns");
+      var maxColumns = dynamicColumn.getAttribute("max-columns");
+      var htmlTagElements = dynamicColumn.querySelector(".original-content").querySelectorAll(htmlTags);
+      var breakPointIndexes = [];          
 
-        htmlTagElements.forEach((el, index) => {
-            if ((el.innerHTML) === "[column-break]") {
-                breakPointIndexes.push(index);
-            }
-        });
-        breakPointIndexes.push(htmlTagElements.length - 1);
-        var columnOutput = dynamicColumn.querySelector(".column-output")
-        var columns = [];
-        var currentBreakPoint = 0;
-        var columnCount = breakPointIndexes.length;
-
-        var htmlElementsArray = Array.prototype.slice.call(htmlTagElements);
-        breakPointIndexes.forEach((breakPoint, j) => {
-            columns.push(htmlElementsArray.slice(currentBreakPoint, breakPoint));
-
-            if (j == breakPointIndexes.length - 1) {
-                columns[columns.length - 1].push(htmlElementsArray[htmlElementsArray.length - 1]);
-            } else {
-                currentBreakPoint = breakPoint + 1;
-            }
-        });
-
-        columnOutput.innerHTML = "";
-
-        var columnStructure = document.createElement("div");
-        columnStructure.setAttribute("class", "column-structure");
-
-        columns.forEach(column => {
-            console.log(column)
-            var reformedHTML = "";
-            column.forEach(htmlElement => {
-                reformedHTML += htmlElement.outerHTML;
-            });
-
-
-            let columnDiv = document.createElement("div");
-            columnDiv.setAttribute("class", "column");
-            columnDiv.innerHTML = reformedHTML;
-            columnStructure.appendChild(columnDiv);
-        });
-
-        columnOutput.appendChild(columnStructure);
-
-        if (columnCount > maxColumns) {
-            dynamicColumn.setAttribute("element-error", "true");
-            dynamicColumn.setAttribute("column-error", "max-column");
-        } else if (columnCount < minColumns) {
-            dynamicColumn.setAttribute("element-error", "true");
-            dynamicColumn.setAttribute("column-error", "min-column");
-        } else {
+      htmlTagElements.forEach((el, index) =>{
+        if ( (el.innerHTML).includes('[column-break]') ){
+          breakPointIndexes.push(index);
         }
-        if (columnCount > maxColumns || columnCount < minColumns) {
-            dynamicColumn.setAttribute("element-error", "true");
-            dynamicColumn.setAttribute("column-error", "min-column");
-            dynamicColumn.setAttribute("column-error", "max-column");
-        } else {
-            dynamicColumn.setAttribute("element-error", "false");
-            dynamicColumn.setAttribute("column-error", "");
+      });
+      
+      breakPointIndexes.push(htmlTagElements.length-1);
+
+      var columnOutput = dynamicColumn.querySelector(".column-output");
+      var columns = [];
+      var currentBreakPoint = 0;
+      var columnCount = breakPointIndexes.length;
+      var htmlElementsArray = Array.prototype.slice.call(htmlTagElements);          
+      
+      breakPointIndexes.forEach((breakPoint, j) => {
+        columns.push(htmlElementsArray.slice(currentBreakPoint, breakPoint));
+
+        if(j == breakPointIndexes.length-1){
+          columns[columns.length-1].push(htmlElementsArray[htmlElementsArray.length-1]);
+        } else{
+          currentBreakPoint = breakPoint+1;
         }
+      });          
+
+      columnOutput.innerHTML = "";
+
+      var columnStructure = document.createElement("div");
+      columnStructure.setAttribute("class", "column-structure");
+
+      columns.forEach(column => {
+        var reformedHTML = "";
+        column.forEach(htmlElement => {
+          reformedHTML += htmlElement.outerHTML;
+        });
+
+
+        let columnDiv = document.createElement("div");
+        columnDiv.setAttribute("class", "column");
+        columnDiv.innerHTML = reformedHTML;
+        columnStructure.appendChild(columnDiv);
+      });
+
+      columnOutput.appendChild(columnStructure);
+
+      if (columnCount > maxColumns) {
+        dynamicColumn.classList.add('overflow');
+        dynamicColumn.setAttribute("element-error", "true");
+        dynamicColumn.setAttribute("column-error", "max-column");
+      } else if (columnCount < minColumns) {
+        dynamicColumn.classList.add('overflow');
+        dynamicColumn.setAttribute("element-error", "true");
+        dynamicColumn.setAttribute("column-error", "min-column"); 
+      } else {
+        dynamicColumn.classList.remove('overflow');
+        dynamicColumn.setAttribute("element-error", "false");
+        dynamicColumn.setAttribute("column-error", "");
+      }
     });
-}           
+  }   
